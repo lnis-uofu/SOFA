3c685311e96f06a0a7f3eed196e4eb10b81ce619
tangxifan <tangxifan@gmail.com>
2020-11-28 15:22:50 -0700

    [Testbench] Bug fix for the ccff testbench to sync with latest netlist

--- TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/ccff_test_post_pnr_include_netlists.v
+++ TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/ccff_test_post_pnr_include_netlists.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccf049ae0a3470c7c96242c7ff31a4bc05344a22e09d515fb9c35cfb378c1bc5
+oid sha256:f3da10525b0dff707611379ed6ae1348c294eae1a8cd31499ce2e35dde3beaab
 size 1329
