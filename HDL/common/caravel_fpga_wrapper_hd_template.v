899018d503427098ffe7c611a012c5dfa1713d58
tangxifan <tangxifan@gmail.com>
2020-11-29 12:38:25 -0700

    [HDL] Bug fix in wrapper template

--- HDL/common/caravel_fpga_wrapper_hd_template.v
+++ HDL/common/caravel_fpga_wrapper_hd_template.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19ddb50cdba53273276a27fc4964cd6691fd8e2ab73ca79b91478d29a5770ac9
+oid sha256:0970910e47d809b8748958d5828b580dca8735205da533069af7ce6cb5a8ba34
 size 3246
