c70d5ac4f0dbf622dd3317b67553986feb1f8231
tangxifan <tangxifan@gmail.com>
2020-11-30 09:42:31 -0700

    [Testbench] Add ccff test wrapper testbench and include netlist

---
+++ TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/ccff_test_post_pnr_wrapper.v
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed96f64d008f69b13c352cefea841509291d29edf65fdad1f8d5ae5f2a972499
+size 29471
---
+++ TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/ccff_test_post_pnr_wrapper_include_netlists.v
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06bb3d6a1ee52298d3ac59ad73f16132b268c013b3cb411d0dfe6f033f6aed36
+size 1488
