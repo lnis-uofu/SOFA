2b40d5fb4bc603bd24835947350311fd0330ccf8
tangxifan <tangxifan@gmail.com>
2020-11-30 09:34:26 -0700

    [HDL] Bug fix

--- TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/scff_test_post_pnr_wrapper_include_netlists.v
+++ TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/scff_test_post_pnr_wrapper_include_netlists.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33fd521a78b634170da832cd9e481ffd8005682dee862c51c359b5edc6978e7a
-size 1391
+oid sha256:c3b64adf49892e8d7e7263273e76b566cb1f0922d404c224d550136d43b49178
+size 1487
