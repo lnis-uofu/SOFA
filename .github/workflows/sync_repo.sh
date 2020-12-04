60060762e54f12a570c94696eda5a2fc54c71289
Ganesh Gore <goreganesh007@gmail.com>
2020-12-04 09:06:17 -0700

    [Action] Replaced destination repo url

--- .github/workflows/sync_repo.sh
+++ .github/workflows/sync_repo.sh
@@ -1,5 +1,5 @@
 
-date >> ../Caravel-SOFA-HD/testfile.txt
+date >> ../Caravel-SOFA-HD/test_log_file.txt
 
 
 # MAGTYPE=mag magic -rcfile ${PDK_ROOT}/sky130A/libs.tech/magic/current/sky130A.magicrc -noc -dnull merge.tcl
