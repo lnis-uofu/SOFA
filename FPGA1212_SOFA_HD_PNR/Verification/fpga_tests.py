13fc082cb3c47fa417290f282b330fbcab1f75b6
Ganesh Gore <goreganesh007@gmail.com>
2020-12-14 01:16:30 -0700

    [SOFA_HD] Updated verification script

--- FPGA1212_SOFA_HD_PNR/Verification/fpga_tests.py
+++ FPGA1212_SOFA_HD_PNR/Verification/fpga_tests.py
@@ -14,162 +14,155 @@ from cocotb.binary import BinaryValue
 from cocotb.log import SimLogFormatter
 from cocotb.clock import Clock
 from cocotb import wavedrom
-from cocotb.utils import get_sim_time
 from cocotb.handle import Force, Release, Deposit
 from cocotb.monitors import Monitor
 from cocotb.scoreboard import Scoreboard
-from cocotb.result import SimTimeoutError, TestFailure, SimTimeoutError, TestSuccess
-from cocotb.triggers import FallingEdge, RisingEdge, Timer, ClockCycles, with_timeout, First
+from cocotb.triggers import FallingEdge, RisingEdge, Timer, ClockCycles
 
 root_logger = logging.getLogger()
 
 
 file_handler = RotatingFileHandler(
     "run.log", maxBytes=(5 * 1024 * 1024), backupCount=2)
+# file_handler.setFormatter(SimLogFormatter())
 root_logger.addHandler(file_handler)
 
-# Caravel interface pin mapping
-FromPinAlias = {
-    "prog_clk": "io_in[37]",
-    "clk": "io_in[36]",
-    "test_en": "io_in[0]",
-    "sc_head": "io_in[26]",
-    "sc_tail": "io_in[11]",
-    "ccff_head": "io_in[12]",
-    "ccff_tail": "io_in[35]"
-}
+
+@cocotb.test()
+async def AutoConfigureTest(dut):
+
+    CFFPaths = CreateCCFFChainPaths(dut)
+    TB = cocotb.os.environ['TB']
+    BitFile = os.path.join(".", "TESTBENCH", TB, "fabric_bitstream.xml")
+    # Create a 10us period clock on port clk
+    clock = Clock(dut.prog_clk_pad, 10, units="ns")
+    cocotb.fork(clock.start())  # Start the clock
+
+    # Initializing Chip with reset
+    dut.pReset_pad <= 1
+    dut.Reset_pad <= 1
+    await FallingEdge(dut.prog_clk_pad)
+    await FallingEdge(dut.prog_clk_pad)
+
+    dut._log.info(f"Testbench {BitFile}")
+
+    # Initializing Chip with reset
+    dut.pReset_pad <= 0
+    dut.Reset_pad <= 0
+
+    AutoConfigure(dut, BitFile, CFFPaths)
+    await RisingEdge(dut.prog_clk_pad)
+    with open("AfterProgramming.data", "w") as fp:
+        for eachPath in CFFPaths:
+            val = eval(eachPath)
+            if "0" in val.value.binstr:
+                dut._log.error(f"Wrong path {eachPath} {val.value}")
+            val = "\n".join(list(val.value.binstr))
+            fp.write(val+"\n")
+    await ClockCycles(dut.prog_clk_pad, 1)
 
 
-def getFromPinAlias(dut, pinName):
-    ''' Get DUT pin from alias '''
-    return eval(f"dut.{FromPinAlias[pinName]}")
+@cocotb.test()
+async def FastConfigureAndResetTest(dut):
+    '''
+    This test configures the fabric with given bitstream and
+    then resets it for one clock cycle and checks for all paths for logice 0
+    '''
+
+    chainLength, CFFPaths = CreateCCFFChainPaths(dut)
+    TB = cocotb.os.environ['TB']
+    BitFile = os.path.join(".", "TESTBENCH", TB, "fabric_bitstream.bit")
+
+    #  Create a 10ns period clock on port clk
+    clock = Clock(dut.prog_clk_pad, 10, units="ns")
+    cocotb.fork(clock.start(start_high=True))  # Start the clock
+    SaveConfiguration(CFFPaths, "BeforeInit.data", style="adjusted")
+
+    # Initializing Chip with reset
+    dut.pReset_pad <= 1
+    dut.Reset_pad <= 1
+    await FallingEdge(dut.prog_clk_pad)
+    await FallingEdge(dut.prog_clk_pad)
+
+    # Initializing Chip with reset
+    dut.pReset_pad <= 0
+    dut.Reset_pad <= 0
+    dut.ccff_head_pad <= 0
+    SaveConfiguration(CFFPaths, "BeforeProgramming.data", style="adjusted")
+
+    await AutoConfigure(dut, BitFile, CFFPaths, chainLength)
+    # await ProgramPhase(dut, BitFile)
+
+    # await RisingEdge(dut.prog_clk_pad)
+    SaveConfiguration(CFFPaths, "AfterProgramming.data", style="adjusted")
+
+    for _ in range(8):
+        await FallingEdge(dut.prog_clk_pad)
+
+    SaveConfiguration(CFFPaths, "After10Clocks.data", style="adjusted")
+
+    # Reset device
+    dut.pReset_pad <= 1
+    await FallingEdge(dut.prog_clk_pad)
+    dut.pReset_pad <= 0
+
+    # Check if all FF are Reset
+    for _, eachModule in CFFPaths.items():
+        for eachPath in eachModule:
+            val = eachPath["obj"].value.binstr
+            if "1" in val:
+                dut._log.error(f"Failed to reset {val} {eachPath['name']}")
+                dut._log.error(eachPath["obj"].value)
+
+    SaveConfiguration(CFFPaths, "AfterReset.data", style="adjusted")
+    await RisingEdge(dut.prog_clk_pad)
 
 
 @cocotb.test()
 async def ConfigChainTestFull(dut):
-    # = = = = = = = Get Design Variable = = = = = = = = = = = = = = = = =
-    PConf = getConfig()
-    clk = getFromPinAlias(dut, "clk")
-    prog_clk = getFromPinAlias(dut, "prog_clk")
-    test_en = getFromPinAlias(dut, "test_en")
-    ccff_head = getFromPinAlias(dut, "ccff_head")
-    ccff_tail = getFromPinAlias(dut, "ccff_tail")
-    PCLK_PERIOD = 10  # in nanoseconds
-    # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-    clk <= 0  # Disable prog clock
-    pclock = Clock(prog_clk, PCLK_PERIOD*0.5, units="ns")
-    cocotb.fork(pclock.start())  # Start the clock
+    DESIGN_NAME = os.environ.get('DESIGN_NAME', 'fpga_top')
+    ProgClockPort = dut.prog_clk_pad if DESIGN_NAME == 'fpga_top' else dut.prog_clk
 
-    # Clock Preamble Ticks 2
-    await ClockCycles(prog_clk, 2)
-
-    # Pass 1 bit logic to CCFF chain
-    ccff_head <= 1
-    await FallingEdge(prog_clk)
-    ccff_head <= 0
-
-    # Check CCFF_tail of each module in sequence
-    CCFFChain = filter(lambda x: not "grid_io" in x, CreateCCFFChain())
-    try:
-        start_ccff_time = get_sim_time(units='ns')
-        for ModuleName in CCFFChain:
-            InstPtr = eval(f"dut.fpga_core_uut.{ModuleName}.ccff_tail")
-
-            # Wait for tick
-            start_time_ns = get_sim_time(units='ns')
-            await with_timeout(FallingEdge(InstPtr), 200*PCLK_PERIOD, 'ns')
-            edge_time_ns = get_sim_time(units='ns')
-
-            # Verify
-            CLKTick = math.ceil((edge_time_ns-start_time_ns)/PCLK_PERIOD)
-            dut._log.info(
-                f"Signal received at {ModuleName} at {CLKTick}")
-            if (CLKTick != 8):
-                TestFailure(
-                    f"Expected 8 ticks on module {ModuleName} received {CLKTick}")
-        end_ccff_time = get_sim_time(units='ns')
-        await ClockCycles(prog_clk, 10)
-        TotalClock = math.ceil((end_ccff_time-start_ccff_time)/PCLK_PERIOD)
-        dut._log.info(f"Simulation Finished in clocks {TotalClock}")
-    except SimTimeoutError:
-        raise TestFailure(f"Failed to receive signal on {ModuleName}")
-
-
-@cocotb.test()
-async def ScanChainTestFull(dut):
-    # = = = = = = = Get Design Variable = = = = = = = = = = = = = = = = =
-    PConf = getConfig()
-    clk = getFromPinAlias(dut, "clk")
-    prog_clk = getFromPinAlias(dut, "prog_clk")
-    test_en = getFromPinAlias(dut, "test_en")
-    sc_head = getFromPinAlias(dut, "sc_head")
-    sc_tail = getFromPinAlias(dut, "sc_tail")
-    CLK_PERIOD = 10  # in nanoseconds
-    # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-
-    prog_clk <= 0  # Disable prog clock
-    clock = Clock(clk, CLK_PERIOD*0.5, units="ns")
+    # Create a 10us period clock on port clk
+    clock = Clock(ProgClockPort, 10, units="ns")
     cocotb.fork(clock.start())  # Start the clock
 
-    # Clock Preamble Ticks 2
-    await ClockCycles(clk, 2)
+    # Initializing Chip with reset
+    dut.pReset_pad <= 1
+    dut.Reset_pad <= 1
+    await FallingEdge(ProgClockPort)
+    await FallingEdge(ProgClockPort)
 
-    # Setup control signals
-    await FallingEdge(clk)
-    test_en <= 1
+    # Initializing Chip with reset
+    dut.pReset_pad <= 0
+    dut.Reset_pad <= 0
 
-    # Pass 1 bit logic to SCFF chain
-    sc_head <= 1
-    await FallingEdge(clk)
-    sc_head <= 0
+    # Push single bit in chain
+    dut.ccff_head_pad <= 1
+    await FallingEdge(ProgClockPort)
+    dut.ccff_head_pad <= 0
 
-    try:
-        start_scff_time = get_sim_time(units='ns')
-        for X in range(1, 1+PConf["FPGA_SIZE_X"]):
-            Yrange = range(1, 1+PConf["FPGA_SIZE_X"])
-            Yrange = reversed(Yrange) if (X % 2) else Yrange
-            for Y in Yrange:
-                ModuleName = f"grid_clb_{X}__{Y}_"
-                PinName = "SC_OUT_BOT" if (X % 2) else "SC_OUT_TOP"
-                InstPtr = eval(f"dut.fpga_core_uut.{ModuleName}.{PinName}")
-                # Wait for tick
-                start_time_ns = get_sim_time(units='ns')
-                await with_timeout(FallingEdge(InstPtr), 50*CLK_PERIOD, 'ns')
-                edge_time_ns = get_sim_time(units='ns')
-
-                # Verify
-                CLKTick = math.ceil((edge_time_ns-start_time_ns)/CLK_PERIOD)
-                dut._log.info(
-                    f"Signal received at {ModuleName} at {CLKTick}")
-                if (CLKTick != 8):
-                    TestFailure(
-                        f"Expected 8 ticks on module {ModuleName} received {CLKTick}")
-        end_scff_time = get_sim_time(units='ns')
-        TotalClock = math.ceil((end_scff_time-start_scff_time)/CLK_PERIOD)
-        await ClockCycles(clk, 10)
-        dut._log.info(f"Simulation Finished in clocks {TotalClock}")
-        dut._log.info(f"Per Grid {TotalClock/(PConf['FPGA_SIZE_X']**2)}")
-    except SimTimeoutError:
-        raise TestFailure(f"Failed to receive signal on {ModuleName}")
+    # Check CCFF_tail of each module in sequence
+    CCFFChain = CreateCCFFChain()
+    ChainTrack = 0
+    PrevCapture = 0
+    for i in range(sys.maxsize):
+        await FallingEdge(ProgClockPort)
+        if eval(CCFFChain[ChainTrack]) == 1:
+            dut._log.info(
+                f"{ChainTrack} Received at {CCFFChain[ChainTrack]}" +
+                f" after {i-PrevCapture} Clocks at [{i}]")
+            PrevCapture = i
+            ChainTrack += 1
+            if ChainTrack == len(CCFFChain):
+                break
+    dut._log.info(f"Total bitstream length {i} bits")
 
 
 # ###================================================================
 # = = = = = = = = = = Utils Functions = = = = = = = = = = = = = = = =
 # ###================================================================
-
-
-def getConfig():
-    """
-    return config.sh varaibles with default values
-    """
-    return {
-        "TECHNOLOGY": os.environ.get('TECHNOLOGY', 'skywater'),
-        "PROJ_NAME": os.environ.get('PROJ_NAME', None),
-        "DESIGN_STYLE": os.environ.get('DESIGN_STYLE', "hier"),
-        "FPGA_SIZE_X": int(os.environ.get('FPGA_SIZE_X', 0)),
-        "FPGA_SIZE_Y": int(os.environ.get('FPGA_SIZE_Y', 0)),
-    }
 
 
 @cocotb.coroutine
@@ -262,7 +255,8 @@ def CreateCCFFChain():
         glob.glob("./TaskConfigCopy/*_task/arch/fabric_key.xml")[0])
     items = mydoc.getElementsByTagName('key')
     for elem in items:
-        CCFFChain.append(elem.attributes['alias'].value)
+        inst = elem.attributes['alias'].value
+        CCFFChain.append(f"dut.fpga_core_uut.{inst}.ccff_tail")
     return CCFFChain
 
 
