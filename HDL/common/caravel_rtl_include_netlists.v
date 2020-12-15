d663d240cb89addc8a386dd018d1f5e6aef0ee96
tangxifan <tangxifan@gmail.com>
2020-12-15 16:14:01 -0700

    [HDL] Add include netlist for Caravel RTL netlists

--- HDL/common/caravel_rtl_include_netlists.v
+++ HDL/common/caravel_rtl_include_netlists.v
@@ -7,7 +7,7 @@
 
 `define USE_POWER_PINS 1
 
-`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/HDL/common/caravel_defines.v"
+`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/defines.v"
 
 ////////////////////////////////////
 // Skywater standard cell netlists
@@ -22,17 +22,8 @@
 `include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/PDK/sky130A/libs.ref/sky130_fd_sc_hvl/verilog/sky130_fd_sc_hvl.v"
 `include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/PDK/sky130A/libs.ref/sky130_fd_sc_hvl/verilog/primitives.v"
 
-// Gate-level netlists 
-`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/gl/DFFRAM.v"
-`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/gl/caravel.v"
-`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/gl/chip_io.v"
-`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/gl/digital_pll.v"
-`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/gl/mgmt_core.v"
-`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/gl/user_id_programming.v"
-
 // Use RTL codes for the following module as the gate-level netlists are buggy
 // in handling power pins
-`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/defines.v"
 `include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/mgmt_protect.v"
 `include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/mgmt_protect_hv.v"
 `include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/gpio_control_block.v"
@@ -42,3 +33,9 @@
 `include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/storage.v"
 `include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/user_proj_example.v"
 `include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/sram_1rw1r_32_256_8_sky130.v"
+`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/DFFRAM.v"
+`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/caravel.v"
+`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/chip_io.v"
+`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/digital_pll.v"
+`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/mgmt_core.v"
+`include "/research/ece/lnis/USERS/tang/github/caravel/verilog/rtl/user_id_programming.v"
