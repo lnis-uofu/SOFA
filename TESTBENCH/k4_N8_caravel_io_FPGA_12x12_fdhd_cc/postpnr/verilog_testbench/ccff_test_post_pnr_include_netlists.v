501c2799ed65fc24a7ea6c9400ea274befb070e4
tangxifan <tangxifan@gmail.com>
2020-12-14 20:27:22 -0700

    [Testbench] Rename testbench to be consistent with post-PnR netlist path change

--- TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/ccff_test_post_pnr_include_netlists.v
+++ TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/postpnr/verilog_testbench/ccff_test_post_pnr_include_netlists.v
@@ -1,3 +1,28 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5b1dbfb9b52a4e87e544773b46710111d2a53c24922de3e10eefd82c7d280f0b
-size 1199
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Netlist Summary
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Wed Nov 11 16:01:30 2020
+//-------------------------------------------
+//----- Time scale -----
+`timescale 1ns / 1ps
+
+// Design parameter for FPGA I/O sizes
+`define FPGA_IO_SIZE 144
+
+// Design parameter for FPGA bitstream sizes
+`define FPGA_BITSTREAM_SIZE 67960
+
+// ------ Include simulation defines -----
+`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/TESTBENCH/k4_N8_caravel_io_FPGA_12x12_fdhd_cc/prepnr/verilog_testbench/define_simulation.v"
+
+`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/HDL/common/skywater_function_verification.v"
+
+// ------ Include Skywater cell netlists -----
+`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/TESTBENCH/common/post_pnr_fpga_cells.v"
+
+// ------ Include fabric top-level netlists -----
+`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/FPGA1212_SOFA_HD_PNR/fpga_top/fpga_top_icv_in_design.pt.v"
+
+`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/TESTBENCH/common/ccff_test_post_pnr_v1.0.v"
