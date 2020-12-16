b5fa0733a271025e0594d571bc5d579a8d42ac3c
tangxifan <tangxifan@gmail.com>
2020-12-16 11:43:02 -0700

    [Testbench] Start build caravel scff test

--- TESTBENCH/common/caravel_qlsofa_hd_scff_test_rtl_include_netlists.v
+++ TESTBENCH/common/caravel_qlsofa_hd_scff_test_rtl_include_netlists.v
@@ -6,7 +6,7 @@
 `timescale 1ns / 1ps
 
 // Include caravel gate-level netlists
-`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/HDL/common/caravel_rtl_include_netlists.v"
+`include "/research/ece/lnis/USERS/tang/github/skywater-openfpga/HDL/common/caravel_qlsofa_hd_rtl_include_netlists.v"
 
 // Include testbench files
 `include "/research/ece/lnis/USERS/tang/github/caravel/verilog/dv/caravel/user_proj_example/io_ports/io_ports_tb.v"
